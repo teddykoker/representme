7d6d0a0180601e67384421e6dd5a6058e43e82b8
Jay Maxwell <jaymaxwell17@gmail.com>
2015-05-31 21:08:44 -0400

    Leader and Bill struct revised
    Leader and Bill struct revised

---
+++ RepresentMe_beta/Bill.swift
@@ -0,0 +1,19 @@
+//
+//  Bill.swift
+//  RepresentMe_beta
+//
+//  Created by Jay Maxwell on 5/31/15.
+//  Copyright (c) 2015 Teddy Koker. All rights reserved.
+//
+
+import Foundation
+struct bill{
+    var chamber: String
+    var introduced_on: String
+    var  official_title: String
+    var short_title: String
+    var  popular_title: String
+    var summary: String
+    var sponsor: String
+    
+}
--- RepresentMe_beta/Leader.swift
+++ RepresentMe_beta/Leader.swift
@@ -8,7 +8,22 @@
 
 import Foundation
 struct Leader {//I'll add more to this
+    var firstName: String
+    var middle_name: String?
     var lastName: String
+    var name_suffix: String? //not allways there
     var title: String
     var bioguideId: String
+    var party: String
+    var state: String
+    var state_rank: String? //senate only
+    var district: String? //house only
+    var birthday: String
+    var term_start: String
+    var term_end: String
+    var phone: String
+    var website: String
+    var contact_form: String
+   
+    
 }
