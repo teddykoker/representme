52b699184f80e9bb92ad3171e2034d5594f409b6
Jay Maxwell <jaymaxwell17@gmail.com>
2015-06-09 14:59:18 -0400

    smallChanges

--- RepresentMe_beta/BillViewController.swift
+++ RepresentMe_beta/BillViewController.swift
@@ -7,3 +7,39 @@
 //
 
 import Foundation
+import UIKit
+
+class BillViewController: UIViewController{
+
+    @IBOutlet weak var textView: UITextView!
+    var dataList: DataList!
+    var bill: Bill!
+    
+    override func viewDidLoad() {
+        
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        super.viewDidLoad()
+        configureView()
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func configureView(){
+        dataList = DataList.list
+        bill = dataList.bills[dataList.selectedLeader]
+        
+        textView.text = bill.official_title
+        
+        
+    
+    
+    }
+
+
+}
